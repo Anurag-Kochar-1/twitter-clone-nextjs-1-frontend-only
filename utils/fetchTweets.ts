127f5e5deec03a1ad42df701e1846cb4b3162b40
Anurag <anuragmarketing101@gmail.com>
2022-10-30 20:48:03 +0530

    changed-to-prod

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,10 +1,10 @@
 import { Tweet } from "../typings"
 
 export const fetchTweets = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getTweets`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_PROD}api/getTweets`)
     const data = await res.json()
     const tweets:Tweet[] = data.tweets
 
-    console.log("fetchTweets function is running from fetchTweets.ts");
+    console.log("fetchTweets function is runningggg from fetchTweets.ts");
     return tweets
 }
